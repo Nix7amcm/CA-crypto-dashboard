10e1d961289eb5a6d1471bb6dc546130caad8aa4
NixyMc⚡ <65091582+Nix7amcm@users.noreply.github.com>
2023-11-17 15:51:22 +0000

    🚀 Ready to deploy
    update backend URL and .env, ready to deploy frontend

--- frontend/src/backendURL.js
+++ frontend/src/backendURL.js
@@ -1,7 +1,7 @@
 // **** Develop Local:
-const backendURL = 'http://localhost:8000'
+// const backendURL = 'http://localhost:8000'
 
 // **** Deployed:
-// const backendURL = 'http://render.....'
+const backendURL = process.env.REACT_APP_BACKEND_URL;
 
 export default backendURL;
